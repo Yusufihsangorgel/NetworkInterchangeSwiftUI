724f259945fd377759050f9d8fc69b952e8c9144
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-12-14 14:59:12 +0300

    Initial Commit

---
+++ NetworkInterchangeSwiftUI/NetworkInterchangeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangeSwiftUIApp.swift
+//  NetworkInterchangeSwiftUI
+//
+//  Created by Yusuf İhsan Görgel on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
